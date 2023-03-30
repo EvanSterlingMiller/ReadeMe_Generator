368e5e7c8576030271009349d4c0cba69526a2fc
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-03-30 09:36:15 -0400

    more js code

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const inquirer = require("inquirer")
 const fs = require("fs")
 
 const prompts = [
-
-    
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project you want displayed?"
+    },
+    {
+        type: "input",
+        name: "introduction",
+        message: "Give a introduction to your product and a brief description:",
+    },
+    {
+        type: "input",
+        name: "instalation",
+        message: "Provide an overview of the instalation process and what technologies are used:",
+    },
 ]
